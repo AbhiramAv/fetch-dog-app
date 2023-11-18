096794699d88be4f3bcbe8483245930855192a4c
AbhiramAv <abhiramav04@gmail.com>
2023-11-18 00:48:10 -0500

    Implemented Filter by Breed

--- src/ResultsPage.tsx
+++ src/ResultsPage.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from '../src/api';
 import DogCard from './DogDetails';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 interface Dog {
   id: string;
@@ -12,70 +14,79 @@ interface Dog {
 }
 
 interface DogsResponse {
-    data: Dog[]
+  data: Dog[];
+}
+
+interface BreedRequestParams {
+  size: number;
+  from: number;
+  sort: string;
+  breeds?: string[] | null;
 }
 
 interface SearchComponentState {
   dogs: Dog[];
+  breeds: string[];
   error?: string;
   currentPage: number;
   sortDirection: 'asc' | 'desc';
-  selectedBreed: string | null;
+  selectedBreeds: string[] | null;
 }
 
-interface DogSearchResponse{
+interface DogSearchResponse {
   data: {
     next: string;
     prev: string;
     resultIds: string[];
     total: number;
-  }
+  };
 }
+
 class DogSearchComponent extends Component<{}, SearchComponentState> {
   constructor(props: {}) {
     super(props);
 
     this.state = {
       dogs: [],
+      breeds: [],
       error: undefined,
       currentPage: 1,
       sortDirection: 'asc',
-      selectedBreed: null,
+      selectedBreeds: [],
     };
   }
 
   componentDidMount() {
     // Fetch data when the component mounts
     this.fetchDogs();
+    this.fetchBreeds();
   }
+
+  fetchBreeds = async () => {
+    try {
+      const response = await api.get('/dogs/breeds');
+      this.setState({ breeds: response.data });
+    } catch (error) {
+      console.error('Failed to fetch breeds', error);
+    }
+  };
 
   fetchDogs = async () => {
     try {
-      const { currentPage, sortDirection, selectedBreed } = this.state;
+      const { currentPage, sortDirection, selectedBreeds } = this.state;
 
-      const response : DogSearchResponse = await api.get('/dogs/search', {
+      const response: DogSearchResponse = await api.get('/dogs/search', {
         params: {
-          size: 100, 
+          size: 100,
           from: (currentPage - 1) * 100,
           sort: `breed:${sortDirection}`,
-          breed: selectedBreed,
-        },
+          breeds: selectedBreeds,
+        } as BreedRequestParams,
       });
 
-      const dog_id_list : string[]= []
-
-      console.log('result.resultIds?', response);
-      response.data.resultIds.forEach((item) => {
-        console.log('hit here??', item);
-        dog_id_list.push(item as string);
-      });
-
+      const dog_id_list: string[] = response.data.resultIds.slice(0, 100);
 
       const dogDetails: DogsResponse = await api.post('/dogs', dog_id_list);
-      
-      console.log('what are we getting', JSON.stringify(dogDetails));
-
-
       this.setState({ dogs: dogDetails.data as Dog[], error: undefined });
     } catch (error) {
       this.setState({ dogs: [], error: 'Failed to fetch dogs' });
@@ -95,14 +106,14 @@ class DogSearchComponent extends Component<{}, SearchComponentState> {
     );
   };
 
-  handleBreedFilterChange = (breed: string | null) => {
-    this.setState({ selectedBreed: breed, currentPage: 1 }, this.fetchDogs);
+  handleBreedFilterChange = (event: React.ChangeEvent<{}>, value: string | null) => {
+    const selectedBreeds = value ? [value] : [];
+    this.setState({ selectedBreeds, currentPage: 1 }, this.fetchDogs);
   };
 
   render() {
-    const { dogs, error, currentPage, sortDirection, selectedBreed } = this.state;
+    const { dogs, breeds, error, currentPage, sortDirection } = this.state;
 
-    console.log('hit here?', JSON.stringify(this.state))
     return (
       <div>
         <h2>Dog Search Results</h2>
@@ -117,10 +128,13 @@ class DogSearchComponent extends Component<{}, SearchComponentState> {
           </label>
           <label>
             Filter by Breed:
-            <input
-              type="text"
-              value={selectedBreed || ''}
-              onChange={(e) => this.handleBreedFilterChange(e.target.value)}
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={breeds}
+              sx={{ width: 300 }}
+              onChange={this.handleBreedFilterChange}
+              renderInput={(params) => <TextField {...params} label="Breed" />}
             />
           </label>
         </div>
